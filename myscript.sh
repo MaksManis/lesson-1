49a00e57f8f92af0a7597d0b4a749d7e2495b6ad
MaksManis <imaxmanis@gmail.com>
2023-07-03 17:01:40 +0300

    first

---
+++ myscript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd starship
+mkdir BattleCruiser
+mkdir Battleship
+mkdir Dreadnought
+cd Battleship
+touch Abaddon.txt
+touch Raven.txt
+touch Megathron.txt
+touch Tempest.json
+touch Rattlesnake.json
+mkdir Barghest
+mkdir Bhaalgorn
+mkdir Machariel
+ls -l
+mv Abaddon.txt Barghest/Abaddon.txt
+mv Raven.txt Barghest/Raven.txt
